079ceda63b1970d751946cb95b3d5aef4d4b7041
R S K <66393045+mn9-29@users.noreply.github.com>
2022-10-18 21:47:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# qr-appearance
+
+Discord: (https://discord.gg/bEs6cn3225)
+
+# Credits
+* [Ktoś](https://github.com/Ktos93) Thank you for agreeing to use the script [rdr_creator]
+
+# converted
+* [R S K](https://github.com/mn9-29) It was converted to QR completely
+
+# Dependencies
+* [QRCore](https://github.com/QRCore-RedM-Re)
+* [qr_menu](https://github.com/QRCore-RedM-Re/qr_menu)
+* [qr_cloths](https://github.com/QRCore-RedM-Re/qr-clothes)
+
+## Information
+
+It is still in development and we are happy to have developers involved in developing this script
+
+## Issues and Suggestions
+Please use the [Github](https://github.com/QRCore-RedM-Re) issues system to report issues or make suggestions, when making suggestion, please keep `[Suggestion]` in the title to make it clear that it is a suggestion or join the 
+[discord](https://discord.gg/bEs6cn3225).
---
+++ server/sv_main.lua
@@ -0,0 +1,67 @@
+local QRCore = exports['qr-core']:GetCoreObject()
+
+RegisterServerEvent('qr_appearance:SaveSkin')
+AddEventHandler('qr_appearance:SaveSkin', function(skin)
+    local _skin = skin
+    local _source = source
+    local encode = json.encode(_skin)
+    local Player = QRCore.Functions.GetPlayer(source)
+    local citizenid = Player.PlayerData.citizenid
+    local license = QRCore.Functions.GetIdentifier(source, 'license')
+    TriggerEvent("redemrp_db:retrieveSkin", citizenid, license, function(call)
+        if call then
+            MySQL.Async.execute("UPDATE playerskins SET `skin` = ? WHERE `citizenid`= ? AND `license`= ?", {encode, citizenid, license})
+        else
+            MySQL.Async.insert('INSERT INTO playerskins (citizenid, license, skin) VALUES (?, ?, ?);', {citizenid, license, encode})
+        end
+    end)
+    TriggerClientEvent('qr-spawn:client:setupSpawnUI', source, encode, true)
+end)
+
+RegisterServerEvent('qr_appearance:SetPlayerBucket')
+AddEventHandler('qr_appearance:SetPlayerBucket', function(b)
+print(b)
+print(GetPlayerRoutingBucket(source))
+   SetPlayerRoutingBucket(source, b)
+end)
+
+RegisterServerEvent('qr_appearance:LoadSkin')
+AddEventHandler('qr_appearance:LoadSkin', function()
+    local _source = source
+    local User = QRCore.Functions.GetPlayer(source)
+    local citizenid = User.PlayerData.citizenid
+    local license = QRCore.Functions.GetIdentifier(source, 'license')
+    local skins = MySQL.Sync.fetchAll('SELECT * FROM playerskins WHERE citizenid = ? AND license = ?', {citizenid, license})
+    if skins[1] then
+        local skin = skins[1].skin
+        local decoded = json.decode(skin)
+        TriggerClientEvent("qr_appearance:ApplySkin", _source, decoded)
+    else
+        TriggerClientEvent("qr_appearance:OpenCreator", _source)
+    end
+end)
+
+AddEventHandler('redemrp_db:retrieveSkin', function(citizenid, license, callback)
+    local Callback = callback
+    local skins = MySQL.Sync.fetchAll('SELECT * FROM playerskins WHERE citizenid = ? AND license = ?', {citizenid, license})
+    if skins[1] then
+        Callback(skins[1])
+    else
+        Callback(false)
+    end
+end)
+
+RegisterServerEvent("qr_appearance:deleteSkin")
+AddEventHandler("qr_appearance:deleteSkin", function(license, Callback)
+    local _source = source
+    local id
+    for k, v in ipairs(GetPlayerIdentifiers(_source)) do
+        if string.sub(v, 1, string.len("steam:")) == "steam:" then
+            id = v
+            break
+        end
+    end
+    local Callback = callback
+    MySQL.Async.fetchAll('DELETE FROM playerskins WHERE `citizenid`= ? AND`license`= ?;', {id, license})
+end)
+
